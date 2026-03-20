9188cd92d30cb2702a1e52bc417cd99fd8cff8aa
poopaapopa <dimma.dort@gmail.com>
2026-03-20 23:46:39 +0300

    header without API

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,19 @@
 import styles from './header.module.scss';
+import logo from '../../assets/logo_168x92.png';
 
 function Header() {
 
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
+      <div className={styles.header__left}>
+        <img src={logo} alt="Tumladen Logo" className={styles.header__logo} />
+        <h1 className={styles.header__title}>TUMLADEN</h1>
+      </div>
 
-    </div>
+      <div className={styles.header__right}>
+        <button className={styles.header__loginBtn}>Войти</button>
+      </div>
+    </header>
   )
 }
 
