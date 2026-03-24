db20119a3849c556c9b99e6a11f57aafa7fad253
poopaapopa <dimma.dort@gmail.com>
2026-03-24 16:07:26 +0300

    сука саси нахуй ты в названиях классов изъёбываешься

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -12,26 +12,24 @@ const Sidebar = ({ onCreateClick }: SidebarProps) => {
     <div className={styles.sidebar}>
       <div className={styles.sidebar__title}>Комнаты</div>
       <div className={styles.sidebar__list}>
-
-        <div className={styles['room-card']}>
-          <img src={castleImage} alt="##" className={styles['room-card__image']} />
-          <div className={styles['room-card__body']}>
-            <div className={styles['room-card__name']}>Название карточки</div>
-            <div className={styles['room-card__settings']}>Описание настроек игры в комнате</div>
-            <div className={styles['room-card__footer']}>
-              <div className={styles['room-card__status']}>Ожидание игроков</div>
-              <div className={styles['room-card__capacity']}>
-                <span className={styles['room-card__count']}>4/5</span>
+        <div className={styles.roomCard}>
+          <img src={castleImage} alt="##" className={styles.roomCard__image} />
+          <div className={styles.roomCard__body}>
+            <div className={styles.roomCard__name}>Название карточки</div>
+            <div className={styles.roomCard__settings}>Описание настроек игры в комнате</div>
+            <div className={styles.roomCard__footer}>
+              <div className={styles.roomCard__status}>Ожидание игроков</div>
+              <div className={styles.roomCard__capacity}>
+                <span className={styles.roomCard__count}>4/5</span>
                   {}
-                <Users size={18} strokeWidth={2.5} className={styles['room-card__icon']} />
+                <Users size={18} strokeWidth={2.5} className={styles.roomCard__icon} />
               </div>
             </div>
           </div>
         </div>
-
       </div>
 
-      <button className={styles['sidebar__create-button']} onClick={onCreateClick}>Создать</button>
+      <button className={styles.sidebar__createButton} onClick={onCreateClick}>Создать</button>
     </div>
   )
 }
